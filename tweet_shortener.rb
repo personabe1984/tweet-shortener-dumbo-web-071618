8574ddbd119570b47315048d81856ff868b3507f
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-11 21:06:25 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,67 @@
-# Write your code here.
+def dictionary
+  look_it_up = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_arr = tweet.split
+  
+  tweet_arr.collect! do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  tweet_arr.join(" ")
+end
+
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+=begin
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + '...'
+  else
+    tweet
+  end
+end
+=end
+
+
+
+test_string = "Hey too, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+puts word_substituter(test_string)
+
+def test(tweet)
+  tweet.split.collect do |word|
+    puts word
+  end
+  
+  dictionary.keys
+end
+
+#puts test(test_string)
