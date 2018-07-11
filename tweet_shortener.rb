ee12bb26ad4ad79b80122373897e8033debe3aa2
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-11 21:17:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -50,7 +50,7 @@ end
 
 
 test_string = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
-puts shortened_tweet_truncator(test_string)
+puts shortened_tweet_truncator(test_string).length
 
 =begin
 def test(tweet)
